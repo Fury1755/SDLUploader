0d3109287a26554dec2fb667fd496b27d930ecf1
Fury1755 <robbstark1755@gmail.com>
2026-07-06 17:55:32 +0800

    feat(pdf_instance.py): added a class 'PDFInstance' that uses everything else in file_splitter to split and save pdf files on the hard disk

---
+++ src/file_splitter/pdf_instance.py
@@ -0,0 +1,71 @@
+"""
+This module holds the class statement_splitter.
+It is a sequential, incremental state machine.
+"""
+
+import os
+import re
+
+from pymupdf import Document, Page
+
+from src.ocr_engine.base import OCREngine
+from src.file_splitter.text_parser import rules_engine
+from src.file_splitter.os_utils import create_folder
+
+
+class PDFInstance:
+    """
+    This class is a pdf instance that grows in size, then flushes itself and saves its contents
+    as a new file when the criteria is reached.
+    It is a state machine that creates a new pdf file when the conditions are reached.
+    """
+
+    def __init__(self, ocr_engine: OCREngine, runtime_parent_dir: str):
+        # this is an existing instance of an initialized OCR engine
+        self._ocr_engine = ocr_engine
+        self._output_folder = create_folder(runtime_parent_dir)
+        self._page_buffer: list[Page] = []
+        self._current_name: str | None = None
+
+    def _flush(self):
+        # guard against empty buffers, accessing indexes of empty list will crash
+        if not self._page_buffer:
+            return
+
+        new_doc = Document()
+        first_page = self._page_buffer[0]
+        last_page = self._page_buffer[-1]
+
+        # satisfy type checker
+        assert first_page.number is not None
+        assert last_page.number is not None
+
+        new_doc.insert_pdf(
+            first_page.parent, from_page=first_page.number, to_page=last_page.number
+        )
+
+        # assert a name exists
+        assert self._current_name is not None
+
+        safe_name = re.sub(r'[<>:"/\\|?*]', "_", self._current_name)
+        file_path = os.path.join(self._output_folder, f"SDL_{safe_name}.pdf")
+        new_doc.save(file_path)
+        new_doc.close()
+
+        # reset attributes
+        self._page_buffer = []
+        self._current_name = None
+
+    def split_statements(self, doc: Document):
+        for page_number, page_text in self._ocr_engine.process_doc(doc):
+            metadata = rules_engine.get_metadata(page_text)
+            page = doc[page_number]
+
+            if metadata.name:
+                if metadata.name != self._current_name:
+                    self._flush()
+                    self._current_name = metadata.name
+
+            self._page_buffer.append(page)
+
+        self._flush()  # flush the last statement
--- src/main.py
+++ src/main.py
@@ -7,7 +7,8 @@ import logging
 import pymupdf
 
 from src.ocr_engine.tesseract_engine import TesseractEngine
-from src.config import PDF_PATH, TESS_DATA_PATH
+from src.file_splitter.pdf_instance import PDFInstance
+from src.config import PDF_PATH, TESS_DATA_PATH, RUNTIME_PARENT_DIR
 
 logging.basicConfig(
     level=logging.INFO,
@@ -16,11 +17,6 @@ logging.basicConfig(
 
 pdf = pymupdf.open(PDF_PATH)
 
-# text_stream is a generator that returns
-#  the corresponding iterable when called.
-if TESS_DATA_PATH is None:
-    raise RuntimeError("TESS_DATA_PATH missing")
 tesser_eng = TesseractEngine(TESS_DATA_PATH)
-for page_number, page_text in tesser_eng.process_doc(pdf):
-    logging.info("Processed page %s", page_number)
-    print(page_text)
+pdf_splitter = PDFInstance(tesser_eng, RUNTIME_PARENT_DIR)
+pdf_splitter.split_statements(pdf)
